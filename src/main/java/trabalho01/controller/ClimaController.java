265a218cbdfd942157f429cd151bbc4077ae8a32
Alequis <alequis01@gmail.com>
2017-08-18 21:25:09 -0300

    Adicionado metodo que le arquivo binario

--- src/main/java/trabalho01/controller/ClimaController.java
+++ src/main/java/trabalho01/controller/ClimaController.java
@@ -1,34 +1,85 @@
 package trabalho01.controller;
 
+import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import trabalho01.model.ClimaDoDia;
 
 public class ClimaController {
 
-	int byteRead;
-	private InputStream inputStream;
-	private OutputStream outputStream;
+    private ArrayList<ClimaDoDia> leBinario(String path) throws FileNotFoundException, IOException, ParseException{
+        DataInputStream dis = new DataInputStream(new FileInputStream(path));
+        
+        dis.readByte();
+        boolean continua = true;
+        ArrayList<ClimaDoDia> listaClima = new ArrayList<ClimaDoDia>();
+        
+        while(continua){
+            String data = "";
+            String direcao = "";
+            int velocidade;
+            int indicePluviometrico;
+            Double temperatura;
+            
+            try{
+                for(int i = 0; i < 11; i++)
+                    data += (char)dis.readByte();
 
-	public void lerAquivo(String path1, String Path2) throws IOException {
-		inputStream = new FileInputStream(path1);
-		outputStream = new FileOutputStream(Path2);
+                data = data.trim();
+                
+                for(int i = 0; i < 4; i++)
+                    direcao += (char)dis.readByte();
 
-		while ((byteRead = inputStream.read()) != -1) {
-			outputStream.write(byteRead);
-		}
-	}
-
-	public static void main(String[] args) {
-		ClimaController a = new ClimaController();
-		try {
-			a.lerAquivo("//home//bmo//DadosMeteorologicos-Exemplo2.dat",
-					"//home//bmo//DadosMeteorologicos-Exemplo22.txt");
-		} catch (IOException e) {
-			// TODO Auto-geFSFnerated catch block
-			e.printStackTrace();
-		}
-	}
+                velocidade = dis.readInt();
+                indicePluviometrico = dis.readInt();
+                temperatura = dis.readDouble();
+                
+                
+                Date date = formataData(data);
+                
+                ClimaDoDia clima = new ClimaDoDia(date, direcao, velocidade, indicePluviometrico, temperatura);
+                listaClima.add(clima);
+                dis.readByte();
+            }catch(EOFException eo){
+                continua = false;
+            }
+        }
+        dis.close();
+        return listaClima;
+    }
+    
+    public static Date formataData(String data) throws ParseException { 
+	if (data == null || data.equals(""))
+            return null;
+        
+        Date date = null;
+        DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        date = formatter.parse(data);
+        return date;
+    }
+    
+    public static void main(String[] args){
+        ClimaController climaController = new ClimaController();
+        ArrayList<ClimaDoDia> listaClima = new ArrayList<ClimaDoDia>();
+        try {
+            listaClima = climaController.leBinario("C:\\Users\\Alequis\\Dropbox\\Furb\\Ciência da Computação\\3º Semestre\\Programação II\\Trabalhos\\Trabalho 1\\DadosMeteorologicos-Exemplo.dat");
+        } catch (IOException ex) {
+            Logger.getLogger(ClimaController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(ClimaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        for(ClimaDoDia c : listaClima){
+            System.out.println(c);
+        }
+    }
 }
---
+++ src/main/java/trabalho01/model/ClimaDoDia.java
@@ -0,0 +1,80 @@
+package trabalho01.model;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import trabalho01.commons.AplicationModel;
+
+public class ClimaDoDia extends AplicationModel implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    Date data;
+    String ventoDirecao;
+    int ventoVelocidade;
+    int indicePluviometrico;
+    double temperatura;
+
+    public ClimaDoDia() {
+    }
+
+    public ClimaDoDia(Date data, String ventoDirecao, int ventoVelocidade, int indicePluciometrico, double temperatura) {
+        this.data = data;
+        this.ventoDirecao = ventoDirecao;
+        this.ventoVelocidade = ventoVelocidade;
+        this.indicePluviometrico = indicePluciometrico;
+        this.temperatura = temperatura;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getVentoDirecao() {
+        return ventoDirecao;
+    }
+
+    public void setVentoDirecao(String ventoDirecao) {
+        this.ventoDirecao = ventoDirecao;
+    }
+
+    public int getVentoVelocidade() {
+        return ventoVelocidade;
+    }
+
+    public void setVentoVelocidade(int ventoVelocidade) {
+        this.ventoVelocidade = ventoVelocidade;
+    }
+
+    public int getIndicePluviometrico() {
+        return indicePluviometrico;
+    }
+
+    public void setIndicePluviometrico(int indicePluviometrico) {
+        this.indicePluviometrico = indicePluviometrico;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+    
+    @Override
+    public String toString(){
+        SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        String conteudo = df.format(data) + "-" + ventoDirecao + "-" + 
+                ventoVelocidade + "-" + indicePluviometrico + "-" + temperatura;
+        return conteudo;
+    }
+}
